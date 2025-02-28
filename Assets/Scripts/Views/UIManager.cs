5683b715a1dcce62dfcf04b3c791915814220999
ABY <abiuqp@gmail.com>
2025-02-27 19:29:22 -0500

    Ajustes Visuales

--- Assets/Scripts/Views/UIManager.cs
+++ Assets/Scripts/Views/UIManager.cs
@@ -6,7 +6,7 @@ public class UIManager : MonoBehaviour
 {
     public GameObject mainPanel;
     public GameObject panelRegistro;
-    public GameObject panelNiveles;
+    public GameObject panelNivel1; // Nuevo panel agregado
     public GameObject[] panelesJuego;
 
     void Start()
@@ -16,15 +16,18 @@ public class UIManager : MonoBehaviour
 
     public void ShowPanel(GameObject panelToShow)
     {
+        // Desactivar todos los paneles
         mainPanel.SetActive(false);
         panelRegistro.SetActive(false);
-        panelNiveles.SetActive(false);
-        
+        panelNivel1.SetActive(false);
+        //panelJuegoFrutas1.SetActive(false); // Desactivar el nuevo panel
+
         foreach (var panel in panelesJuego)
         {
             panel.SetActive(false);
         }
 
+        // Activar el panel solicitado
         panelToShow.SetActive(true);
     }
 }
